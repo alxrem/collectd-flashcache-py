db979416a2080bdab9715a5bf42762d6ad92b5a6
Alexey Remizov <alexey@remizov.org>
2015-11-14 01:18:22 +0300

    Fixed processing of IgnoreSelected directive

--- flashcache.py
+++ flashcache.py
@@ -21,6 +21,7 @@ class Config:
     DMSETUP = '/sbin/dmsetup'
     DEVICES = set()
     IGNORE_SELECTED = False
+    MAPPINGS = None
 
 
 def config_callback(conf):
@@ -30,35 +31,30 @@ def config_callback(conf):
         elif node.key == 'DMSetup':
             Config.DMSETUP = node.values[0]
         elif node.key == 'IgnoreSelected':
-            value = node.values[0].lower()
-            if value in ['true', 'yes', '1']:
-                ignore_selected = True
-            elif value in ['false', 'no', '0']:
-                ignore_selected = False
-            else:
-                log('{0} is wrong value of IgnoreSelected'.format(value))
+            Config.IGNORE_SELECTED = node.values[0]
         else:
-            log('Unknown confiuration key {0}'.format(node.key))
+            log('Ignoring unknown config key "{0}".'.format(node.key))
 
 def init_callback():
-    all_devices = set([entry
-                       for entry in os.listdir(PROC_ROOT)
-                       if os.path.isdir(os.path.join(PROC_ROOT, entry))])
+    mappings = detect_mappings()
 
-    if Config.DEVICES:
-        for device in Config.DEVICES - all_devices:
-            log('Unknown flashcache device {0}. '
-                'Examine {1}.'.format(device, PROC_ROOT))
-        if Config.IGNORE_SELECTED:
-            Config.DEVICES = all_devices - Config.DEVICES
-        else:
-            Config.DEVICES = all_devices & Config.DEVICES
+    if not Config.DEVICES:
+        Config.MAPPINGS = mappings
+        return
+
+    all_devices = set(mappings.keys())
+
+    unknown_devices = Config.DEVICES - all_devices
+    for device in unknown_devices:
+        log('Unknown flashcache device {0}.'.format(device, PROC_ROOT))
+
+    if Config.IGNORE_SELECTED:
+        devices = all_devices - Config.DEVICES
     else:
-        Config.DEVICES = all_devices
+        devices = Config.DEVICES - unknown_devices
+    Config.MAPPINGS = dict([(device, mappings[device]) for device in devices])
 
-    Config.DM_DEVICES = detect_dm_devices()
-
-def detect_dm_devices():
+def detect_mappings():
     try:
         p = Popen([Config.DMSETUP, 'table'], stdout=PIPE, stderr=STDOUT)
     except OSError:
@@ -66,21 +62,22 @@ def detect_dm_devices():
     rc = p.wait()
     if rc != 0:
          raise Exception('dmsetup execution error')
-    return dict([('{0}+{1}'.format(ssd, disk), dmdev)
-                 for dmdev, ssd, disk
-                 in DMSETUP_RE.findall(p.stdout.read())])
+    return dict([(dmdev, '{0}+{1}'.format(ssd, disk))
+                for dmdev, ssd, disk
+                in DMSETUP_RE.findall(p.stdout.read())])
 
 def read_callback():
-    for device in Config.DEVICES:
+    for device in Config.MAPPINGS:
         for name in ['flashcache_stats', 'flashcache_errors']:
-            with open(os.path.join(PROC_ROOT, device, name)) as stats:
+            stats_file = os.path.join(PROC_ROOT, Config.MAPPINGS[device], name)
+            with open(stats_file) as stats:
                 dispatch_stats(stats.read(), device)
 
 def dispatch_stats(stats, device):
     for metric, val in STATS_RE.findall(stats):
         value = collectd.Values()
         value.plugin = 'flashcache'
-        value.plugin_instance = Config.DM_DEVICES[device]
+        value.plugin_instance = device
         value.type = 'gauge'
         value.type_instance = metric.replace(' ', '_')
         value.values = [int(val)]
@@ -88,7 +85,7 @@ def dispatch_stats(stats, device):
 
 def log(message, level='warning'):
     level_method = getattr(collectd, level)
-    level_method('flashcache plugin: {0}'.format(message))
+    level_method('flashcache module: {0}'.format(message))
 
 
 collectd.register_config(config_callback)
